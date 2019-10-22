4a13c30ea15c3ab839eda19bbb12116e2a5c5ef7
Guillaume Bollard <gubolb@gigl.polymtl.ca>
2019-10-22 18:33:18 -0400

    Ajout coverage TP2 (non testé)

--- tp2/src/packet_gen.cpp
+++ tp2/src/packet_gen.cpp
@@ -1,6 +1,7 @@
 #include <systemc.h>
 #include "packet_gen.h"
 #include "my_random_obj.h"
+#include "input_coverage.h"
 
 // SC_THREAD(generate)
 // sensitive(next_packet)
@@ -13,9 +14,11 @@ void packet_gen::generate( void )
 
 	crave::init("crave.cfg");
 	my_rand_obj obj;
+	input_coverage obj2;
 	TestBase tb;
 
-	while (i < 8)
+	//while (i < 8)
+	while (obj2.get_inst_coverage() < 50) {  // le pourcentage de couverture que l'on veut obtenir --> à calculer 
 	{
  		packet_ready = false;
 		cout << "GEN : attente du bus pret" << endl;
@@ -24,6 +27,7 @@ void packet_gen::generate( void )
 		wait(50, SC_NS);
 		
     	CHECK(obj.next()); // Génération des attributs aléatoires du prochain paquet
+		obj2.sample(obj.copro_value(), obj.sort_dir_value(), obj.data_order_value()/*, obj.address_value()*/); //indiquer à la couverture l'échantillon testé
 
 		// Générer un nouveau paquet et l'envoyer au coprocesseur
 		// dont le numéro a été généré aléatoirement
@@ -42,5 +46,8 @@ void packet_gen::generate( void )
 		delete pkt;
 		i++;
 	}
+
+	fc4sc::global::coverage_save("coverage_results.xml");
+	std::cout << "nombre d'execution requise pour 81.25%: " << obj2.nb_de_cov << std::endl;
 	
 }
